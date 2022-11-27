afd126401fa12542b78649c096bd4395f8fc6706
Yangjisung <ygji6406@gmail.com>
2022-11-27 23:45:37 +0900

    Router_practice

--- 코딩애플/ShoppingMall/my-app/src/App.js
+++ 코딩애플/ShoppingMall/my-app/src/App.js
@@ -2,13 +2,17 @@ import './app.css'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import ItemList from './components/ItemList.jsx'
+// import ItemList from './components/ItemList.jsx'
 import Detail from './components/Detail.jsx'
 import { Routes, Route, link, Outlet } from "react-router-dom";
 import Event from './components/Event'
+import {useState} from 'react'
+import data from './data'
 
 function App() {
   // let navigate = useNavigate();
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
 
@@ -24,23 +28,44 @@ function App() {
         <Route path="/" element={
           <>
             <div className="main-bg">
-              <ItemList />
+              <ItemList shoes={shoes} />
             </div>
           </>
         }
         />
-
-        <Route path="/detail" element={<Detail />}/>
-        <Route path="*" element={<div>없는 페이지</div>}/>
+        <Route path="/detail" element={<Detail shoes={shoes}/>} />
+        <Route path="/detail/:id" element={<Detail shoes={shoes}/>} />
+        <Route path="/detail/:id" element={<Detail shoes={shoes}/>} />
+        <Route path="*" element={<div>없는 페이지</div>} />
         <Route path="/about" element={<div>about</div>} />
-        
+
         <Route path="/event" element={<Event />} >
-          <Route path='one' element={<div>첫 주문시 양배추즙 서비스</div>}/>
-          <Route path='two' element={<div>생일기념 쿠폰받기</div>}/>
+          <Route path='one' element={<div>첫 주문시 양배추즙 서비스</div>} />
+          <Route path='two' element={<div>생일기념 쿠폰받기</div>} />
         </Route>
       </Routes>
     </div>
   );
 }
 
+function ItemList(props) {
+  return (
+    <div className="container">
+      <div className="row">
+        {
+          props.shoes.map((v, i) => {
+            return (
+              <div className="col-md-4">
+                <img src={`https://codingapple1.github.io/shop/shoes${i + 1}.jpg`} width="80%" />
+                <h4>{v.title}</h4>
+                <p>{v.content}</p>
+                <p>{v.price}</p>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  )
+}
 export default App;
--- 코딩애플/ShoppingMall/my-app/src/components/Detail.jsx
+++ 코딩애플/ShoppingMall/my-app/src/components/Detail.jsx
@@ -1,19 +1,45 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import styled from 'styled-components'
+import { useEffect, useState } from 'react';
 
-export default function Detail() {
+// let Yellowbtn = styled.button`
+//   background: ${ props => props.bg };
+//   color: ${ props => props.bg == 'blue' ? 'white': 'black' };
+//   padding: 10px;
+// `
+
+export default function Detail(props) {
+
+    useEffect( ()=> {
+
+    });
+
+    
+
+    let [count, setCount] = useState(0);
+
+    let {id} = useParams();
+    let 찾은상품 = props.shoes.find(x => x.id == id);
+    console.log(id)
+
   return (
       <div className="container">
+            {/* <Yellowbtn bg="blue">버튼</Yellowbtn>
+            <Yellowbtn bg="yellow">버튼</Yellowbtn> */}
+            <button onClick={() => { setCount(count+1)}}> 버튼 </button>
           <div className="row">
               <div className="col-md-6">
                   <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
               </div>
               <div className="col-md-6">
-                  <h4 className="pt-5">상품명</h4>
-                  <p>상품설명</p>
-                  <p>120000원</p>
+                  <h4 className="pt-5">{props.shoes[id].title}</h4>
+                  <p>{props.shoes[id].content}</p>
+                  <p>{props.shoes[id].price}</p>
                   <button className="btn btn-danger">주문하기</button>
               </div>
           </div>
       </div> 
   )
-}
+};
+
