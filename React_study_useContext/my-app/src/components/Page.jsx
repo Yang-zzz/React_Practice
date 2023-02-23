a4b40737bb5fbcf1a23902e67c71fc691009900e
Yangjisung <ygji6406@gmail.com>
2023-02-23 17:27:24 +0900

    React : useContext + contextAPI 예제

---
+++ React_study_useContext/my-app/src/components/Page.jsx
@@ -0,0 +1,15 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Content from "./Content.jsx";
+
+function Page() {
+  return (
+    <div className="page">
+      <Header />;
+      <Content />;
+      <Footer />;
+    </div>
+  );
+}
+
+export default Page;
